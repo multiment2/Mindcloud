627e9c3298384686755617c12c1fbe9cf1165d3e
multiment <multiment@gmail.com>
2022-01-06 21:08:26 +0300

    Add "home" blueprint

--- dispetcher_app.py
+++ dispetcher_app.py
@@ -2,13 +2,15 @@
 Здесь подключаем все конечные точки и чертежи.
 '''
 
-from flask import Flask 
+from flask import Flask, redirect
 from API_rpc.API_rpc import rpc_bp
+from home.index import home
 
 app=Flask(__name__)
 
+app.register_blueprint(home)
 app.register_blueprint(rpc_bp)
 
 @app.route('/')
 def index():
-    return "Hello, I am Index page"
+    return redirect('/home')
