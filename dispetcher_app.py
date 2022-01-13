aa43d0400faba24a903de1409bb32232230697c5
multiment <multiment@gmail.com>
2022-01-13 21:37:23 +0300

    Add Blueprint as 'home' for home and index pages

--- dispetcher_app.py
+++ dispetcher_app.py
@@ -3,8 +3,8 @@
 '''
 
 from flask import Flask, redirect, current_app
-from API_rpc.API_rpc import api, db
-#from API_rpc.rpc_api_data_base import db
+from API_rpc.API_rpc import api, db #Объект базы данных надо
+# импортировать оттуда, где он ипользуется.
 from home.index import home
 import config_app_global
 
@@ -13,11 +13,10 @@ app=Flask(__name__)
 app.config.from_pyfile('config_app_global.py')
 
 app.add_url_rule('/api', 'api', api.as_view(), methods = ['POST'])
-#app.add_url_rule('/api', 'index')
 
-#current_app.config([config_app_global])
-#app.register_blueprint(home)
-#app.register_blueprint(rpc_bp)
+app.register_blueprint(home)
+app.add_url_rule('/', 'home')
+
 
 db.init_app(app)   #Инициализируем базу данных
 
@@ -25,6 +24,6 @@ with app.app_context():
     db.create_all(bind = 'mindcloud_api')
 
 
-@app.route('/')
+#@app.route('/')
 def index():
     return redirect('/home')
--- home/index.py
+++ home/index.py
@@ -3,5 +3,10 @@ from flask import Blueprint
 home=Blueprint('home', __name__)
 
 @home.route('/')
-def index():
+def start_page():
     return "I am Start page"
+
+
+@home.route('/index')
+def index_page():
+    return "I am Index page"
