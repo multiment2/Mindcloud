3fde961becd3ad6b01b2227c6d9294a9fa7745bb
multiment2 <51969777+multiment2@users.noreply.github.com>
2022-10-24 05:29:16 +0000

    Пересмотрел депозиторий. Удалил лишние файлы.

--- dispetcher_app.py
+++ dispetcher_app.py
@@ -7,30 +7,22 @@ from API_rpc.API_rpc import api, db #Объект базы данных надо
 # импортировать оттуда, где он ипользуется.
 from home.index import home
 import config_app_global
-from flask_cors import CORS, cross_origin
+
 
 app=Flask(__name__)
 
-CORS(app)
 
 app.config.from_pyfile('config_app_global.py')
 
 app.add_url_rule('/api', 'api', api.as_view(), methods = ['POST'])
 
 app.register_blueprint(home)
-#app.add_url_rule('/home', 'home')
+
 
 
 db.init_app(app)   #Инициализируем базу данных
 
 with app.app_context():
     db.create_all(bind = 'mindcloud_api')
-
-
-@app.route('/')
-def index():
-    return "Start page"
-
-
 
 
--- home/index.py
+++ home/index.py
@@ -1,17 +1,10 @@
 from flask import Blueprint, render_template
 
 
-home=Blueprint('home', __name__, static_folder='static',url_prefix='/home', template_folder='templates_dir')
+home=Blueprint('home', __name__, static_folder='static_dir', template_folder='templates_dir')
 
 
 
 @home.route('/')
 def start_page():
     return "I am Start page"
-
-
-@home.route('/game')
-def start_game():
-    return render_template('index.html')
-
-
