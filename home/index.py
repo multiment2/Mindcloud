2adc8def868f7d4658856556a2195a277489521f
multiment2 <51969777+multiment2@users.noreply.github.com>
2022-10-26 10:35:34 +0000

    Add home, help and help pages with css-style files

--- home/index.py
+++ home/index.py
@@ -6,5 +6,14 @@ home=Blueprint('home', __name__, static_folder='static_dir', template_folder='te
 
 
 @home.route('/')
+@home.route('/home')
 def start_page():
-    return render_template("base.html")
+    return render_template("home_page.html")
+
+@home.route('/help')
+def help_page():
+    return render_template("help_page.html")
+
+@home.route('/notebook')
+def notebook():
+    return render_template("notebook.html")
