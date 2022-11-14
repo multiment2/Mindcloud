1a5c9908aab47d55ac9aeb1977d166113a48096f
multiment2 <51969777+multiment2@users.noreply.github.com>
2022-11-14 10:45:38 +0000

    Update js script

--- home/static_dir/notebook_script.js
+++ home/static_dir/notebook_script.js
@@ -2,8 +2,10 @@ var body_request = JSON.stringify({
     json-rpc: '2.0',
     method: method,
     params: params,
-    id: id
+    id: 10
 })
+
+var params = new FormData(view-content)
 
 var response = fetch('api', {
     method: "POST",
@@ -14,6 +16,31 @@ var response = fetch('api', {
     
 });
 
+<<<<<<< HEAD
 function save_stick {
     var sticker = new FormData
+=======
+
+function save-stick ("create_new_stick", params){
+    var response = fetch('api', {
+        method: "POST";
+        headers:{
+            'Content-Type': 'Application/json';
+        },
+        body: body-request;
+        
+    });
+    return alert (response.json().message)
+}
+
+function get_one_stick() {
+    fetch('api', {
+        method: "POST";
+        headers:{
+            'Content-Type': 'Application/json';
+        },
+        body: body-request;
+        
+    });
+>>>>>>> 5246c33 (Update js script)
 }
