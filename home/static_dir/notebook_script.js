6dd107c3f6f6ddc3cc42060ccac7a07620fa35d3
multiment <multiment@gmail.com>
2022-11-15 22:12:20 +0300

    Вариант удачного запроса

--- home/static_dir/notebook_script.js
+++ home/static_dir/notebook_script.js
@@ -1,31 +1,32 @@
-<<<<<<< HEAD
+
 var body_request = {
     "jsonrpc": '2.0',
-    "method": method,
-    "params": params,
-    "id": 10
+    "method": "get_all_stick",
+    "params": [],
+    "id": 1
 }
 
-var params = new FormData(view-content);
-=======
+
+/*
 var body_request = (method, params, id) => JSON.stringify ({
     "jsonrpc": '2.0',
     "method": method,
     "params": params,
     "id": id
 })
+*/
 
->>>>>>> bc70beb (Настройка скрипта javascript)
 
 
-var response = fetch('/api', {
-    method: "POST",
+var response = fetch('api', {
+    method: 'POST',
     headers:{
         'Content-Type': 'Application/json'
     },
-    body: body_request
+    body: JSON.stringify(body_request)
 });
 
-function get_all_stick (response = ("get_all_stick", [], "0")) {
-        alert(response.json)
+
+function get_all_stick (response) {
+        console.log(response => response.json())
 }
