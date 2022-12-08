77c4397a950909671bbc1d0359b020a851dc22f3
Михаил Сорокин <multiment@gmail.com>
2022-12-08 10:40:55 +0000

    Add function for get one stick

--- home/static_dir/notebook_script.js
+++ home/static_dir/notebook_script.js
@@ -1,7 +1,3 @@
-
-
-
-
 /*
 var body_request = (method, params, id) => JSON.stringify ({
     "jsonrpc": '2.0',
@@ -10,39 +6,62 @@ var body_request = (method, params, id) => JSON.stringify ({
     "id": id
 })
 */
-  
-function get_all_stick () {   
-    var body_request = JSON.stringify ({
-    "jsonrpc": '2.0',
-    "method": "get_all_stick",
-    "params": [],
-    "id": 1
-});
-    fetch('api', {
-    method: 'POST',
-    headers:{
-        'Content-Type': 'Application/json'
-    },
-    body: body_request})
-    .then(response => response.json())
-    .then(body => make_links(body.result));
-    };
 
-function make_links(result){
+function get_all_stick() {               //Функция для загрузки всех ссылок на стики
+    var body_request = JSON.stringify({
+        "jsonrpc": '2.0',
+        "method": "get_all_stick",
+        "params": [],
+        "id": 1
+    });
+    fetch('api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'Application/json'
+        },
+        body: body_request
+    })
+        .then(response => response.json())
+        .then(body => make_links(body.result));
+};
+
+function make_links(result) {
 
     let lns = document.createElement('ul');
-    for (let i in result){
+    for (let i in result) {
         let li = document.createElement('li');
         let text = document.createTextNode(result[i].name + result[i].date);
-        
+
         li.setAttribute("id", result[i].id);
         li.addEventListener("click", getID);
         li.appendChild(text);
-        lns.appendChild(li);      
-        }
-    document.getElementById('view-links').appendChild(lns);
+        lns.appendChild(li);
     }
-       
-function getID(event){
-    console.log(this.id);  
+    document.getElementById('view-links').appendChild(lns);
+}
+
+function getID(event) {
+    //console.log(this.id);
+    return event.id;
+};
+
+let id = getID();
+console.log(id);
+
+function make_stick(id){
+    var body_request = JSON.stringify({
+        "jsonrpc": '2.0',
+        "method": "get_one_stick",
+        "params": [id],
+        "id": 1
+    });
+    fetch('api', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'Application/json'
+        },
+        body: body_request
+    })
+        .then(response => response.json())
+        .then(body => console.log(body.result));
 };
